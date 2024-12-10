d6971be9d81b39ced7f196d6dca6cd22e2f3c8ef
rushirajbigscal <rushiraj@bigscal.com>
2024-12-10 17:09:30 +0530

    multiple file download logic added

--- send_script.py
+++ send_script.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import requests
 import os
+import argparse
 from action_functions import *  
 
 app = Flask(__name__)
@@ -9,19 +10,22 @@ app = Flask(__name__)
 def print_payload():
     try:
         payload = request.get_json()
-        url = payload["downloadableFile"]
-        filename = os.path.join('C:\\temp',get_filename(url))
-        print(filename)
-        response = requests.get(url)
-        print(response.status_code)
-        if response.status_code == 200:
-            with open(filename, 'wb') as file:
-                file.write(response.content)
-            print(f"File downloaded at {filename}")
-        else:
-            print("Failed to download the file.")
+        files = payload["files"]
+        print(files)
+        for file in files:
+            url = file["downloadURL"]
+            print(url)
+            filename = os.path.join(default_path,get_filename(url))
+            print(filename)
+            response = requests.get(url)
+            print(response.status_code)
+            if response.status_code == 200:
+                with open(filename, 'wb') as file:
+                    file.write(response.content)
+                print(f"File downloaded at {filename}")
+            else:
+                print("Failed to download the file.")
         
-        print("Request Payload:", payload)
         return jsonify({"message": "Payload received", "Data": payload}), 200
     
     except Exception as e:
@@ -29,4 +33,8 @@ def print_payload():
         return jsonify({"message": "An error occurred while processing the payload."}), 400
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path', required = True, help = 'path')
+    args = parser.parse_args()
+    default_path = args.path
     app.run(host="0.0.0.0",port=5000)
